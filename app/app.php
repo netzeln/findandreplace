7d2967f113a3d6b168084bebb42735bcb88bc78d
Jason Awbrey and nic netzel <jason.s.awbrey@gmail.com>
2016-02-17 11:48:52 -0800

    Add app routes and twig page

--- app/app.php
+++ app/app.php
@@ -1,13 +1,28 @@
-<?php 
+<?php
 
-	require_once __DIR__.'/../vendor/autoload.php'; 
+	require_once __DIR__.'/../vendor/autoload.php';
+	require_once __DIR__.'/../src/WordReplace.php';
 
-	$app = new Silex\Application(); 
+	// use Symfony\Component\Debug\Debug;
+	// Debug::enable();
 
-	$app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views')); 
+	$app = new Silex\Application();
+	// $app['debug'] = true;
 
-	$app->get('/', function(){return 'Hello, World!';}); 
+	$app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
-	return $app; 
+	$app->get('/', function() use ($app)
+	{
+		return $app['twig']->render('findandreplace.html.twig');
+	});
+
+	$app->get('/findandreplace', function() use ($app)
+	{
+		$new_replacement = new WordReplace($_GET['string'],$_GET['word'],$_GET['replacement']);
+		$results = $new_replacement->findAndReplace();
+		return $app['twig']->render('findandreplace.html.twig', array('results' => $results));
+	});
+
+	return $app;
 
 ?>
