d29b3f183b1084a9952a25af2a94f42e710625e4
Jason Awbrey and nic netzel <jason.s.awbrey@gmail.com>
2016-02-17 11:14:58 -0800

    Add all passing tests

--- src/WordReplace.php
+++ src/WordReplace.php
@@ -5,36 +5,58 @@
 		private $word;
 		private $replacement;
 
-		 function getString()
-		 {
-			return $this->string;
+		function __construct($string, $word, $replacement)
+			{
+					$this->string = $string;
+					$this->word = $word;
+					$this->replacement = $replacement;
+			}
+
+		function getString()
+			{
+				return $this->string;
 			}
 
 		function setString($string)
-		{
-			$this->string = $string;
-		}
+			{
+				$this->string = $string;
+			}
 
 		function getWord()
-		{
-		 return $this->word;
-		 }
+			{
+				return $this->word;
+			}
 
-		 function setWord($word)
-		 {
-			 $this->word = $word;
-		 }
+		function setWord($word)
+			{
+			 	$this->word = $word;
+			}
 
-	 function getReplacement()
-	 	{
-			return $this->replacement;
-		}
+		function getReplacement()
+			{
+				return $this->replacement;
+			}
 
 		function setReplacement($replacement)
-		{
-			$this->replacement = $replacement;
-		}
+			{
+				$this->replacement = $replacement;
+			}
 
+		function findAndReplace()
+			{
+				$lengthWord = strlen($this->word);
+				$lengthReplacement = strlen($this->replacement);
+				$newString = $this->string;
+				$offset = 0;
+
+				while(strpos($newString, $this->word, $offset) !==  FALSE)
+				{
+					$start = strpos($newString, $this->word, $offset);
+					$newString = substr_replace($newString, $this->replacement, $start, $lengthWord);
+					$offset = $start + $lengthReplacement;
+				}
+				return $newString;
+			}
 
 	}
  ?>
---
+++ tests/WordReplaceTest.php
@@ -0,0 +1,57 @@
+<?php
+
+	require_once 'src/WordReplace.php';
+
+	class WordReplaceTest extends PHPUnit_Framework_TestCase
+	{
+
+		function test_findAndReplace_oneWord()
+		{
+		//Arrange
+		$test_wordReplace = new WordReplace("cat", "cat", "dog");
+
+		//Act
+		$result = $test_wordReplace->findAndReplace();
+
+		//Assert
+		$this->assertEquals('dog', $result);
+		}
+
+		function test_findAndReplace_multipleWords()
+		{
+		//Arrange
+		$test_wordReplace = new WordReplace("I have a cat and my cat is awesome", "cat", "dog");
+
+		//Act
+		$result = $test_wordReplace->findAndReplace();
+
+		//Assert
+		$this->assertEquals('I have a dog and my dog is awesome', $result);
+		}
+
+		function test_findAndReplace_wordLongerThan3Characters()
+		{
+		//Arrange
+		$test_wordReplace = new WordReplace("I am a crocodilevelociraptor and I like crocodilevelociraptor", "crocodilevelociraptor", "kangaroo");
+
+		//Act
+		$result = $test_wordReplace->findAndReplace();
+
+		//Assert
+		$this->assertEquals('I am a kangaroo and I like kangaroo', $result);
+		}
+
+		function test_findAndReplace_wordWithinReplacement()
+		{
+			//Arrange
+			$test_wordReplace = new WordReplace("I am a bee in a bee kingdom", "bee", "bumblebee");
+
+			//Act
+			$result = $test_wordReplace->findAndReplace();
+
+			//Assert
+			$this->assertEquals('I am a bumblebee in a bumblebee kingdom', $result);
+		}
+	}
+
+?>
